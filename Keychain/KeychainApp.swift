9b7dc2b2acdafa48159cfcffc6df907812897a2a
Jamil Nawaz <jamilnawaz88@gmail.com>
2023-02-14 21:00:05 +0000

    Initial Commit

---
+++ Keychain/KeychainApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainApp.swift
+//  Keychain
+//
+//  Created by Jamil Nawaz on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct KeychainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
